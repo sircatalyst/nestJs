695336dbc5541f4ca2a67d8d5b9d6a7506c63019
sircatalyst <temibami@gmail.com>
2020-07-09 21:12:13 +0100

    POST, LIST, GET, PATCH, DELETE implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
+import { application } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Content-Type', 'text/html')
   getHello(): string {
     return this.appService.getHello();
   }
